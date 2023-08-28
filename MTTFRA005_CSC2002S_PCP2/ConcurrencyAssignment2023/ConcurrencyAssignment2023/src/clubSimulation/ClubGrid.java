3de0392eab78000d7ae0c69f46236ed794f6053c
Francis Mutetwa <francismutetwa@outlook.com>
2023-08-28 19:10:39 +0200

    final changes

--- MTTFRA005_CSC2002S_PCP2/ConcurrencyAssignment2023/ConcurrencyAssignment2023/src/clubSimulation/ClubGrid.java
+++ MTTFRA005_CSC2002S_PCP2/ConcurrencyAssignment2023/ConcurrencyAssignment2023/src/clubSimulation/ClubGrid.java
@@ -3,18 +3,20 @@
 
 package clubSimulation;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 //This class represents the club as a grid of GridBlocks
 public class ClubGrid {
 	private GridBlock [][] Blocks;
 	private final int x;
 	private final int y;
 	public  final int bar_y;
-	
+	//AtomicBoolean flag_capacity= new AtomicBoolean();
 	private GridBlock exit;
 	private GridBlock entrance; //hard coded entrance
 	private final static int minX =5;//minimum x dimension
 	private final static int minY =5;//minimum y dimension
-	
+	 
 	private PeopleCounter counter;
 	
 	ClubGrid(int x, int y, int [] exitBlocks,PeopleCounter c) throws InterruptedException {
@@ -29,6 +31,9 @@ public class ClubGrid {
 		counter=c;
 		}
 	
+	//AtomicBoolean flag_capacity= new AtomicBoolean(counter.overCapacity());
+
+
 	//initialise the grsi, creating all the GridBlocks
 	private  void initGrid(int []exitBlocks) throws InterruptedException {
 		for (int i=0;i<x;i++) {
@@ -69,26 +74,21 @@ public class ClubGrid {
 			return false;
 		return true;
 	}
-	
+	//AtomicBoolean flag_capacity= new AtomicBoolean(counter.overCapacity());
 	public synchronized GridBlock  enterClub(PeopleLocation myLocation) throws InterruptedException  {
 		//Patrons waiting to see if the club limit is reached or the entrance door is occupied.
 		//Maximum number of patrons inside the club should not be exceeded the maximum.
 		counter.personArrived(); //add to counter of people waiting 
-		if (counter.overCapacity() == false)
-		{
-		//counter.personArrived(); //add to counter of people waiting 
+		while(counter.overCapacity()){
+			wait(1);// Wait with 1 second delay for each thread to enter the club
+		}
+		synchronized (myLocation){
 		entrance.get(myLocation.getID());
 		counter.personEntered(); //add to counter
 		myLocation.setLocation(entrance);
 		myLocation.setInRoom(true);
-		}
-		else{
-			wait();
-		
-			//counter.personArrived(); //add to counter of people waiting 
-			myLocation.setInRoom(false);
-		}
 		return entrance;
+		}
 	}
 	
 	//Added synchronized keyword to ensure liveness and to ensure Patrons move block by block.
@@ -118,12 +118,17 @@ public class ClubGrid {
 		return newBlock;
 	} 
 	
-
+	//added the synchronized keyword to make sure that only one thread can access the exit one at a time.
 	public  synchronized void  leaveClub(GridBlock currentBlock,PeopleLocation myLocation)   {
-			currentBlock.release();
+		synchronized(entrance){	
+		synchronized(counter){
+		currentBlock.release();
 			counter.personLeft(); //add to counter
-			myLocation.setInRoom(false);
+			myLocation.setInRoom(false);			
+			//entrance.notify();
 			entrance.notifyAll();
+		}
+	}
 	}
 
 	public synchronized GridBlock getExit() {
